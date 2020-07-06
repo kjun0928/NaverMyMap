fca398bfabc0331e8aa7f5007b02d5b6b1b5c773
kjun0928 <kjun960928@gmail.com>
2020-07-06 12:55:32 +0900

    update

--- app/src/main/java/com/example/bandinlunis/MainActivity.java
+++ app/src/main/java/com/example/bandinlunis/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
 
+import android.graphics.Color;
 import android.graphics.PointF;
 import android.os.Bundle;
 import android.util.Log;
@@ -11,6 +12,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.Toast;
+import android.content.pm.ActivityInfo;
 
 import com.naver.maps.geometry.LatLng;
 import com.naver.maps.map.LocationSource;
@@ -20,8 +22,13 @@ import com.naver.maps.map.NaverMap;
 import com.naver.maps.map.OnMapReadyCallback;
 import com.naver.maps.map.overlay.InfoWindow;
 import com.naver.maps.map.overlay.Marker;
+import com.naver.maps.map.overlay.Overlay;
+import com.naver.maps.map.overlay.OverlayImage;
+import com.naver.maps.map.overlay.PolygonOverlay;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import static android.widget.Toast.*;
 
@@ -31,6 +38,9 @@ public class MainActivity<setPosition> extends AppCompatActivity implements OnMa
     LinearLayout linemenu;
     boolean menulist = false;
     ArrayList<Marker> listMarker = new ArrayList<>();
+    int markerNumber;
+    List<LatLng> coords = new ArrayList<>();
+    PolygonOverlay polygon = new PolygonOverlay();
 
 
     protected void onCreate(Bundle savedInstanceState) {
@@ -62,6 +72,7 @@ public class MainActivity<setPosition> extends AppCompatActivity implements OnMa
 */
 
     }
+
 
     public void BasicB(View v) {
         myMap.setMapType(NaverMap.MapType.Basic);
@@ -102,31 +113,55 @@ public class MainActivity<setPosition> extends AppCompatActivity implements OnMa
         for (Marker marker : listMarker) {
             marker.setMap(null);
         }
+        polygon.setMap(null);
+        coords.clear();
+        markerNumber = 0;
     }
 
-    public void test(LatLng coord) {
+    public void text(LatLng coord) {
         Toast.makeText(this, coord.latitude + ", " + coord.longitude, Toast.LENGTH_SHORT).show();
+        markerNumber+=1;
     }
+
 
     @Override
     public void onMapReady(@NonNull NaverMap naverMap) {
         myMap = naverMap;
+        Marker marker = new Marker();
+        listMarker.add(marker);
+        marker.setPosition(new LatLng(35.945282, 126.682303));
+        marker.setMap(myMap);
 
 
         myMap.setOnMapClickListener(new NaverMap.OnMapClickListener() {
             @Override
             public void onMapClick(@NonNull PointF point, @NonNull LatLng coord) {
-                test(coord);
+                text(coord);
                 Marker marker = new Marker();
                 listMarker.add(marker);
 
-
+                InfoWindow infoWindow = new InfoWindow();
+                infoWindow.setAdapter(new InfoWindow.DefaultTextAdapter(MainActivity.this) {
+                    @NonNull
+                    @Override
+                    public CharSequence getText(@NonNull InfoWindow infoWindow) {
+                        return "좌표:" + coord.latitude + "," + coord.longitude;
+                    }
+                });
+                coords.add(coord);
                 marker.setPosition(coord);
                 marker.setMap(myMap);
+                infoWindow.open(marker);
 
-
+                if (markerNumber >= 3) {
+                    polygon.setCoords(coords);
+                    polygon.setMap(myMap);
+                    polygon.setColor(Color.BLACK);
+                };
             }
         });
+
+
 /*
         myMap.setOnMapClickListener((point, coord) ->
                 Toast.makeText(this, coord.latitude + ", " + coord.longitude, Toast.LENGTH_SHORT).show());
